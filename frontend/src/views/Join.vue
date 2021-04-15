b0fc46bfb5c2c65a9b9af2d408eeec2ad42955e3
wealways <likeletter5658@gmail.com>
2021-04-15 23:51:12 +0900

    [frontend] 기능추가 : 로그인회원가입페이지 레이아웃

---
+++ frontend/src/views/Join.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    회원가입
+    <div>
+      <div>
+        <label for="id">ID</label>
+        <input 
+          v-model="uid"
+          type="text" 
+          id="id"
+        >
+      </div>
+      <div>
+        <label for="password">비밀번호 확인</label>
+        <input 
+          v-model="password"
+          type="password" 
+          id="password"
+        >
+      </div>
+      <div>
+        <label for="passwordConfirm">비밀번호 확인</label>
+        <input 
+          v-model="passwordConfirm"
+          type="passwordConfirm" 
+          id="passwordConfirm"
+        >
+      </div>
+    </div>
+    <button>회원가입</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Join',
+  data(){
+    return {
+      uid:'',
+      password:'',
+      passwordConfirm:''
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
